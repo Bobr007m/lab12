b2b6fe4edb70194a16cc7207303570d410b807fc
Bobr007m <masha_kovaleva_06@mail.ru>
2025-04-18 19:14:40 +0500

    class point

---
+++ List.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+    class List<T>
+    {
+        public Point<T> begin; // начало списка
+        public int Count => throw new NotImplementedException();
+
+
+        public void Add(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Clear()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ Point.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab12
+{
+     class Point <T>
+    {
+        public T? Data { get; set; }
+        public Point <T>? Next { get; set; }
+
+        public Point()
+        {
+            Data = default(T);
+            Next = null;
+        }
+        public Point (T info)
+        {
+            Data = info;
+            Next = null;    
+        }
+
+        public override string ToString()
+        {
+            return Data?.ToString()??"null"; // проверка на нулевую ссылку
+        }
+    }
+}
