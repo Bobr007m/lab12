bf9f9f759c3b8455612bd6027b126389f92ba63c
Bobr007m <masha_kovaleva_06@mail.ru>
2025-04-18 20:16:20 +0500

    class list

--- List.cs
+++ List.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,33 +11,124 @@ namespace lab12
     class List<T>
     {
         public Point<T> begin; // начало списка
-        public int Count => throw new NotImplementedException();
+        public int Count // счетчик количества элементов
+        {
+            get
+            {
+                int count = 0;
+                if (begin == null) return 0;
+                Point<T> current = begin;
+                while (current != null)
+                {
+                    count++;
+                    current = current.Next;
+                }
+                return count;
+            }
+        }
+
+        public List()
+        {
+            begin = null;
+        }
+
 
 
         public void Add(T item)
         {
-            throw new NotImplementedException();
+            Point<T> newPoint = new Point<T>(item);
+            if (begin == null)
+                begin = new Point<T>();
+            else
+                AddToEnd(newPoint);
+        }
+        public void Add(int number, T item)
+        {
+            Point<T> newPoint = new Point<T>(item);
+            if (number > Count + 1)
+                throw new Exception("номер больше, чем количество элементов в списке");
+            if (number == 0) // в начало
+            {
+                AddToBegin(newPoint);
+                return;
+            }
+            int count = 1;
+            Point<T> current = begin;
+            while (current.Next != null)
+            {
+
+            }
+        }
+        public void AddToEnd(Point<T> item)
+        {
+            Point<T> ptr = begin;
+            while (ptr.Next != null)
+                ptr = ptr.Next;
+            ptr.Next = item;
+        }
+        public void AddToBegin(Point<T> item)
+        {
+            item.Next = begin;
+            begin = item;
         }
 
         public void Clear()
         {
-            throw new NotImplementedException();
+            begin = null;
         }
 
         public bool Contains(T item)
         {
-            throw new NotImplementedException();
+            Point<T> current = begin;
+            while (current != null && !current.Data.Equals(item))
+                current = current.Next;
+            return current != null;
         }
 
-        public void CopyTo(T[] array, int arrayIndex)
+        public void CopyTo(T[] array, int arrayIndex) // копирует элементы из коллекции в массив, начиная с определенного индекса
         {
-            throw new NotImplementedException();
-        }
+            if (array == null) throw new Exception("Массив не может быть null");
+            if (arrayIndex < 0) throw new Exception("Индекс не может быть меньше 0");
+            if (array.Length - arrayIndex < Count) throw new Exception("Список не помещается в массив");
+            Point<T> current = begin;
+            int i = arrayIndex;
+            while (current != null && i < array.Length) {
+                array[i++] = current.Data;
+                current = current.Next;
+            }
 
-        public bool Remove(T item)
+            public bool Remove(T item)
+            {
+                if (begin.Data.Equals(item))
+                {
+                    begin = begin.Next;
+                    return true;
+                }
+                else
+                {
+                    Point<T>? current = begin;
+                    while (current.Next != null && !current.Next.Data.Equals(item))
+                    {
+                        current = current.Next;
+                    }
+                    if (current.Next == null)
+                        return false;
+                    else {
+                        current.Next = current.Next.Next;
+                        return true;
+                    }
+                }
+            }
+        }
+        public void PrintList()
         {
-            throw new NotImplementedException();
+            Point<T> current = begin;
+            int count = 1;
+            while (current != null) {
+                Console.WriteLine($"{count}: {current.Data}");
+                current = current.Next;
+                count++;
+            }
         }
-
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,41 @@ namespace lab12
 {
     internal class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
+            for (int i = 0; i < 10; i++) 
+            {
+                list.Add(rnd.Next(100));
+            }
+           list.PrintList();
+            list.Add(100);
+            Console.WriteLine(list.Contains(100));
+            int[] arr = new int[list.Count + 5];
+            list.CopyTo(arr, 1);
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item+ " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("delete last");
+            list.Remove(100);
+            list.PrintList();
+            Console.WriteLine(" add first");
+            list.AddToBegin(new Point<int>(100));
+            list.PrintList();
+            Console.WriteLine("delete first");
+            list.Remove(100);
+            list.PrintList();
+            List<int> list2 = new List<int>();
+            list2.Add(1);
+            list2.Add(2);
+            list2.Add(3);
+            list2.Remove(2);
+            list2.PrintList();
+
+
         }
     }
 }
